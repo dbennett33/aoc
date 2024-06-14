e2cc168aec09ccbe5edec30712d7a8bca6f3d83a
dan <danbennett2k8@gmail.com>
2024-06-14 22:20:25 +0100

    day1

---
+++ src/2015/day1/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var input = Console.ReadLine().ToCharArray();
+
+        var up = input.Count(c => c == '(');
+        var down = input.Count(c => c == ')');
+        
+        var floor = up - down;
+
+        Console.WriteLine(floor);
+        Console.ReadLine();        
+    }
+}
