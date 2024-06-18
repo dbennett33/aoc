5a5e79ecb597eff8f911e3331a08e877f5171665
dbennett33 <danbennett2k8@gmail.com>
2024-06-18 12:26:36 +0100

    aoc

---
+++ src/2023/day1/Program.cs
@@ -0,0 +1,88 @@
+﻿var input = File.ReadAllLines("input.txt");
+
+
+
+var total = 0;
+
+foreach (var line in input)
+{
+
+    var numbers = new List<int>();
+
+    for (var i = 0; i < line.Length; i++)
+    {
+        if (char.IsNumber(line[i]))
+        { 
+            numbers.Add(int.Parse(line[i].ToString()));          
+        }
+        else
+        {
+            for (var j = 2; j < 6; j++)
+            {
+                try
+                {    
+                    switch (line.Substring(i, j).ToLower())
+                    {
+                        case "one":
+                            numbers.Add(1);
+                            //i += (j - 1);
+                            break;
+                        case "two":
+                            numbers.Add(2);
+                            //i += (j - 1);
+                            break;
+                        case "three":
+                            numbers.Add(3);
+                            //i += (j - 1);
+                            break;
+                        case "four":
+                            numbers.Add(4);
+                            //i += (j - 1);
+                            break;
+                        case "five":
+                            numbers.Add(5);
+                            //i += (j - 1);
+                            break;
+                        case "six":
+                            numbers.Add(6);
+                            //i += (j - 1);
+                            break;
+                        case "seven":
+                            numbers.Add(7);
+                            //i += (j - 1);
+                            break;
+                        case "eight":
+                            numbers.Add(8);
+                            //i += (j - 1);
+                            break;
+                        case "nine":
+                            numbers.Add(9);
+                            //i += (j - 1);
+                            break;
+                    }
+                    
+                }
+                catch
+                {
+                   
+                }
+            }
+        }
+
+
+    }
+    try
+    {
+        var combined = 0;
+        combined = int.Parse($"{numbers.First()}{numbers.Last()}");
+        total += combined;
+    }
+    catch
+    {
+    
+    }
+
+}
+
+Console.WriteLine($"Total: {total}");
+Console.ReadLine();
---
+++ src/2023/day2/Program.cs
@@ -0,0 +1,75 @@
+﻿
+internal class Program
+{
+    public static int totalRed = 12;
+    public static int totalGreen = 13;
+    public static int totalBlue = 14;
+    private static void Main(string[] args)
+    {
+        var input = File.ReadAllLines("input.txt");
+        var total = 0;
+
+        foreach (var line in input)
+        {
+            total += GetResult(line);
+        }
+
+        Console.WriteLine(total);
+    }
+
+
+    public class Round
+    {
+        public int r;
+        public int g;
+        public int b;
+    }
+
+    public static int GetResult(string game)
+    {
+        var lowestB = 0;
+        var lowestG = 0;
+        var lowestR = 0;
+
+        var strGame = game.Split(':');
+        var id = int.Parse(strGame[0].Substring(5));
+
+        var strRounds = strGame[1].Split(';').ToList();
+
+        foreach (var r in strRounds)
+        { 
+            var round = new Round();
+
+            foreach (var c in r.Split(','))
+            {
+                var spl = c.Split(' ');
+                if (spl[2] == "blue")
+                {
+                    round.b = int.Parse(spl[1]);
+                }
+                else if (spl[2] == "green")
+                {
+                    round.g = int.Parse(spl[1]);
+                }
+                else if (spl[2] == "red")
+                {
+                    round.r = int.Parse(spl[1]);
+                }
+            }
+            if (round.r > lowestR)
+            {
+                lowestR = round.r; 
+            }
+            if (round.g > lowestG)
+            {
+                lowestG = round.g;
+            }
+            if (round.b > lowestB)
+            {
+                lowestB = round.b;
+            }
+        }
+
+        return (lowestR*lowestB*lowestG);
+    }
+}
